971e0da54940563c0dc785de0a8b5e7d52887db5
SabaPokemon <122475577+SabaPokemon@users.noreply.github.com>
2024-03-04 16:57:54 +0600

    Add files via upload

---
+++ Web/public_home.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>HTML form</title>
+    <style>
+        p{text-align: right;} 
+        h1{text-align: left;}
+    </style>
+</head>
+<body>
+<table border="1" cellspacing="0" align="center" width="80%">
+        <tr>
+        	<th> <h1>XCompany</h1>
+            <p><a href="home.php"> Home </a> | <a href="login.php"> Login </a> | <a href="registration.php"> Registration </a></p>
+ </th>
+            </tr>
+            <tr>
+             <td><h1>Welcome to xCompany</h1></td>
+            </tr>
+            <tr align="center">
+            <td>Copyright-2017</td>
+            </tr>
+            
+</table>     
+</body>
+</html>
