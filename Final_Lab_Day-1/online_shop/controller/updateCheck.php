bc6fb55860da0750ddaf49aee822d6b5ce437983
SabaPokemon <122475577+SabaPokemon@users.noreply.github.com>
2024-04-03 22:47:46 +0600

    Update updateCheck.php

--- Final_Lab_Day-1/online_shop/controller/updateCheck.php
+++ Final_Lab_Day-1/online_shop/controller/updateCheck.php
@@ -11,22 +11,52 @@
         header('location: ../view/management.php');
     }
 
-    function validateName($name) {
-        $trimmedName = trim($name);
+    function validateUsername($username) {
+        $trimmedName = trim($username);
         if ($trimmedName === '' || !ctype_alpha(str_replace(' ', '', $trimmedName))) {
             return false;
         }
         return true;
     }
 
-    if(isset($_POST['update'])){
-    if (!validateName($name)) {
-        echo "invalid name";
-    } elseif (strlen($name)<4) {
-        echo "name has to be atleast in 4 letters";
-    }else {
+    function validateName($name) {
+        $trimmedName = trim($name);
+        if ($trimmedName === '') {
+            return false;
+        }
+        for ($i = 0; $i < strlen($trimmedName); $i++) {
+            if (!(ctype_alpha($trimmedName[$i]) || $trimmedName[$i] === ' ' || $trimmedName[$i] === '-')) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        $status = alterUser($name,$contact,$username,$password);
+    function validateContactNumber($contact) {
+        $trimmedNumber = trim($contact);
+        if (strlen($trimmedNumber) === 11 && ctype_digit($trimmedNumber)) {
+            return true;
+        }
+        return false; 
+    }
+
+
+    if(isset($_POST['update'])){
+        if($name == "" || $contact == "" || $username == "" || $password==""){
+            echo "fill-up all";
+        } elseif (!validateUsername($username)) {
+            echo "invalid username";
+        } elseif (!validateName($name)) {
+            echo "invalid name";
+        }elseif (!validateContactNumber($contact)) {
+            echo "Contact number must be in XXXXXXXXXXX format";
+        }  elseif (!strlen($password)<8) {
+            echo "8 charaters needed in password";
+        }  elseif (strlen($username)<4) {
+            echo "username has to be atleast in 4 letters";
+        }else {
+
+        $status = updateUser($name,$contact,$username,$password);
         if($status){
             header('location: ../view/management.php');
                
